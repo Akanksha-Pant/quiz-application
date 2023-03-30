6c601c53fccd31e0a7f708ce63d63aed45d1e625
AkankshaScaler <akanksha.pant@scaler.com>
2023-03-30 23:49:24 +0530

    configure firebase to app

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdu-cE7lRn6sxRR9evH-h-s_Jx9ynS5kA",
+  authDomain: "quiz-application-59407.firebaseapp.com",
+  projectId: "quiz-application-59407",
+  storageBucket: "quiz-application-59407.appspot.com",
+  messagingSenderId: "307676253667",
+  appId: "1:307676253667:web:3c3ee5b957753c77fce4d6",
+  measurementId: "G-S181JZKCBK"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export {db};
